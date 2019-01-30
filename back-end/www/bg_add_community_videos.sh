ac01652bd5e30717df5fb71cc062afbab7f4150c
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-01-30 15:32:32 -0500

    Fix the background script.

--- back-end/www/bg_add_community_videos.sh
+++ back-end/www/bg_add_community_videos.sh
@@ -2,6 +2,6 @@ sudo screen -X quit
 sudo rm screenlog.0
 
 # For python in conda env
-sudo screen -dmSL "add_videos" bash -c ". '/opt/anaconda3/etc/profile.d/conda.sh'; conda activate video-labeling-tool; python add_community_videos.py"
+sudo screen -dmSL "add_videos" bash -c "export PATH='/opt/miniconda3/bin:$PATH'; . '/opt/miniconda3/etc/profile.d/conda.sh'; conda activate video-labeling-tool; python add_community_videos.py"
 
 sudo screen -ls
