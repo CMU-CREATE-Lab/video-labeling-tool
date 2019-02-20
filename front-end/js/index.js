2f61787fef9dddcdb51ed02920656843bd48e797
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-02-20 14:52:07 -0500

    Add video test on the index page.

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -5,6 +5,7 @@
 
   function init() {
     var video_test_dialog = new edaplotjs.VideoTestDialog();
+    video_test_dialog.startVideoPlayTest(2000);
     var ga_tracker = new edaplotjs.GoogleAnalyticsTracker({
       tracker_id: util.getGoogleAnalyticsId()
     });
