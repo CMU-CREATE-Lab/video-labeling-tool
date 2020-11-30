aeae94b77439da5770a1388ec34cc31011164533
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2020-11-29 22:00:37 -0500

    Update the script for installing packages.

--- back-end/install_packages.sh
+++ back-end/install_packages.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 # Flask server
-pip install flask==1.1.1
-pip install flask-cors==3.0.8
-pip install flask-sqlalchemy==2.4.1
-pip install flask-marshmallow==0.10.1
-pip install marshmallow-sqlalchemy==0.19.0
-pip install flask-migrate==2.5.2
+pip install --upgrade flask==1.1.1
+pip install --upgrade flask-cors==3.0.8
+pip install --upgrade flask-sqlalchemy==2.4.1
+pip install --upgrade flask-marshmallow==0.10.1
+pip install --upgrade marshmallow-sqlalchemy==0.19.0
+pip install --upgrade flask-migrate==2.5.2
 
 # Google login API
-pip install google-api-python-client==1.7.11
+pip install --upgrade google-api-python-client==1.7.11
 
 # Math
-pip install numpy==1.17.3
+pip install --upgrade numpy==1.17.3
 
 # JSON Web Token
-pip install pyjwt==1.7.1
+pip install --upgrade pyjwt==1.7.1
 
 # http related
-pip install requests==2.22.0
+pip install --upgrade requests==2.22.0
 
 # MySQL
-pip install mysqlclient==1.4.4
+pip install --upgrade mysqlclient==1.4.4
 
 # For visualization of system data
-pip install seaborn==0.9.0
+pip install --upgrade seaborn==0.9.0
 
 # Others
-pip install pytz==2019.3
+pip install --upgrade pytz==2019.3
